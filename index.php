43a1d15c59aaa3038ced3b430e9f6fb1206836ef
Aneesh <srivastavaaneesh004@gmail.com>
2020-12-28 17:36:28 +0530

    Fetch data from DB by SQL in PHP

--- index.php
+++ index.php
@@ -8,7 +8,16 @@
 <body>
 <h1>Connect Database to webpage in PHP</h1>
     <?php
+        //Connect DB
         $con = mysqli_connect("localhost", "root", "", "ecommerce") or die(mysqli_error($con));
+        //Use SOLi select Querry and store it in a variable.
+        $select_query = "SELECT id, email_id, first_name FROM users";
+        //Use the querry and store the result in anothet variable.
+        $select_query_result = mysqli_query($con, $select_query) or die(mysqli_error($con));
+        //Function to count the number of rows fetched.
+        $total_rows_fetched = mysqli_num_rows($select_query_result);
+        //Print the number of rows fetched.
+        echo "Rows fetched : ". $total_rows_fetched;
     ?>
 </body>
 </html>
