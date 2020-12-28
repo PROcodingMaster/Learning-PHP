61ae94ff011671d52c4288a54f3bc2bd484178f7
Aneesh <srivastavaaneesh004@gmail.com>
2020-12-28 13:43:49 +0530

    Added comments and HTML

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
- echo " I am aneesh"
- ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // This is a single line comment
+        echo " Echo statement output"
+        /* Multiline comment 
+        Yes, This is a comment too*/
+    ?>
+</body>
+</html>
