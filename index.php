e9b851150843f72302b077ec31d0434661f9e581
Aneesh <srivastavaaneesh004@gmail.com>
2020-12-28 17:43:38 +0530

    print database data on webpage with PHP

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "T5 - DATABASE CONNECTION."; ?></title>
+    <title><?php echo "T6 - SELECT QUERRY WITH PHP."; ?></title>
 </head>
 <body>
 <h1>Connect Database to webpage in PHP</h1>
@@ -18,6 +18,18 @@
         $total_rows_fetched = mysqli_num_rows($select_query_result);
         //Print the number of rows fetched.
         echo "Rows fetched : ". $total_rows_fetched;
+
+        echo '<br><br> $row will have First row of DB.<br>';
+        $row = mysqli_fetch_array($select_query_result); // This function has inbuilt counter that updates itself each time its called.
+        echo $row["id"] . "<br>";
+        echo $row["email_id"] . "<br>";
+        echo $row["first_name"] . "<br>";
+
+        echo '<br>$row will now have 2nd row of DB.<br>';
+        $row = mysqli_fetch_array($select_query_result); // This function has inbuilt counter that updates itself each time its called.
+        echo $row["id"] . "<br>";
+        echo $row["email_id"] . "<br>";
+        echo $row["first_name"] . "<br>";
     ?>
 </body>
 </html>
