d7b1c4f6f16eb7a3dda743f681eeb95e934ffc54
Aneesh <srivastavaaneesh004@gmail.com>
2020-12-28 16:57:32 +0530

    While Loop

--- index.php
+++ index.php
@@ -6,17 +6,13 @@
     <title><?php echo "T4-LOOPS."; ?></title>
 </head>
 <body>
-<h1>T4 Loops</h1>
+<h1>While Loop</h1>
     <?php
-        echo "<h4>Conditional Statement</h4>";
-        $marks = 50;
-        if($marks >= 40){
-            echo "<br>Student has passed.";
+        $counter = 1;
+        while($counter <= 5){
+            echo "$counter" . "<br>";
+            $counter++;
         }
-        else{
-            echo "<br>Student has failed.";
-        }
-        
     ?>
 </body>
 </html>
