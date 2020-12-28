153e67f88230970586c160db6b84e61e35f15e23
Aneesh <srivastavaaneesh004@gmail.com>
2020-12-28 13:59:32 +0530

    Changed title

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo "Learning PHP"; ?></title>
 </head>
 <body>
 <h1>This is Heading 1.</h1>
     <?php
         // This is a single line comment
-        echo " <h2>Echo statement output</h2>"
+        echo " <h2>Echo statement output</h2>";
         /* Multiline comment 
         Yes, This is a comment too*/
     ?>
