1a48bee9fdf16b1e14baa8c53678febd7e141390
Aneesh <srivastavaaneesh004@gmail.com>
2020-12-28 14:03:30 +0530

    add in echo statement

--- index.php
+++ index.php
@@ -4,15 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "Learning PHP"; ?></title>
+    <title><?php echo "Variables, Datatypes and Operators"; ?></title>
 </head>
 <body>
-<h1>This is Heading 1.</h1>
+<h1>THIS IS T2-BASICS.</h1>
     <?php
-        // This is a single line comment
-        echo " <h2>Echo statement output</h2>";
-        /* Multiline comment 
-        Yes, This is a comment too*/
+        echo  18 + 12;
+        
     ?>
 </body>
 </html>
