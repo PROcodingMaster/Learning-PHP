ce7df62658c107c1f1ecff3bf33ec8bf2ccb94f2
Aneesh <srivastavaaneesh004@gmail.com>
2020-12-29 12:12:09 +0530

    Index is empty and resgistration_script.php file created

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "T7 - SELECT QUERRY WITH PHP part 2."; ?></title>
+    <title><?php echo "T8- FORMS"; ?></title>
     
     <!-- Bootstrap link -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -11,50 +11,13 @@
     
 </head>
 <body>
-<h1 style = "text-align : center">Display DB data</h1>
+<h1 style = "text-align : center">Store data into DB using forms</h1>
     <?php
         //Connect DB
         $con = mysqli_connect("localhost", "root", "", "ecommerce") or die(mysqli_error($con));
-        //Use SOLi select Querry and store it in a variable.
-        $select_query = "SELECT id, email_id, first_name FROM users";
-        //Use the querry and store the result in anothet variable.
-        $select_query_result = mysqli_query($con, $select_query) or die(mysqli_error($con));
         
     ?>
     <div class="container">
-        <?php while($row = mysqli_fetch_array($select_query_result)) { ?>
-            
-            <!-- Horizantal Line -->
-            <hr width="100%" color="black"> 
-            <div class="row">
-                <div class="col-sm-3" style = "text-align : center"><h5>User</h5></div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-2">ID</div>
-                <div class="col-sm-10"><?php echo $row["id"]; ?></div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-2">EMAIL</div>
-                <div class="col-sm-10"><?php echo $row["email_id"]; ?></div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-2">First Name : </div>
-                <div class="col-sm-10"><?php echo $row["first_name"]; ?></div>
-            </div>
-            
-            <div class="row">
-                <div class="col-sm-2">Products : </div>
-                <div class="col-sm-10"><?php echo num_of_products_purchased($con, $row['id']); ?></div>
-            </div>
-            <!-- Horizantal Line -->
-            <hr width="100%" color="black"> 
-            <br>
-            <br>
-            <br>
-        <?php } ?>
     </div>
 </body>
     <!-- JS link -->
